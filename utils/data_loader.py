865774fc1c2827a5aa3e4fbcc1e2d23700a6f032
YS-2357 <joungyoungsun20@gmail.com>
2025-03-13 13:50:25 +0900

    파싱인자 제거 원본 복구

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,14 +1,5 @@
-import argparse
 import torch
 from torchvision import datasets, transforms
-
-def parse_args():
-    """
-    파싱 인자 설정
-    """
-    parser = argparse.ArgumentParser(description="Model Training & Evaluation")
-    parser.add_argument("--batch_size", type=int, default=64, help="Batch size for training")
-    return parser.parse_args()
 
 def get_mnist_data_loader(batch_size=64, train=True):
     transform = transforms.Compose([
@@ -18,9 +9,3 @@ def get_mnist_data_loader(batch_size=64, train=True):
     dataset = datasets.MNIST(root='./data/mnist', train=train, transform=transform, download=True)
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
     return data_loader 
-
-if __name__ == "__main__":
-    args = parse_args()
-    train_loader = get_mnist_data_loader(args.batch_size, train=True)
-    test_loader = get_mnist_data_loader(args.batch_size, train=False)
-    print("Data loaders are successfully created!")
